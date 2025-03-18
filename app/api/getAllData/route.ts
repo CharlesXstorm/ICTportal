92360d0133f55c6764f420cfc997596df93206ae
charlesxstorm <charlesukadike44@gmail.com>
2025-03-18 09:06:58 +0100

    api integration

--- app/api/getAllData/route.ts
+++ app/api/getAllData/route.ts
@@ -1,6 +1,5 @@
 import connectToDatabase from "../../../lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
-import { userModel as User } from "../../../lib/models/userModel";
 import { getAllUserData } from "../../../lib/controllers/userControllers";
 
 export async function GET(req: NextRequest) {
@@ -14,11 +13,6 @@ export async function GET(req: NextRequest) {
       if (!userId) {
         throw new Error("No user token found. Please login");
       }
-
-    //   const user = await User.findById(userId);
-    //   if (!user) {
-    //     throw new Error("user doesn't exist");
-    //   }
 
       const getAllUserDataRes = await getAllUserData(req);
       return getAllUserDataRes;
